92e78716dea96a95fc11cdd109a13925f05290f8
dongdong12311 <664560694@qq.com>
2019-08-23 10:20:23 +0800

    加入xyzuod

--- portfolio/summarise.py
+++ portfolio/summarise.py
@@ -5,33 +5,55 @@ Created on Sun Aug 11 22:23:38 2019
 @author: Administrator
 """
 import datetime
+from result_analyer import Cal
+def int_to_datetime(data):
+    year = int(data / 10000)
+    day = data % 100
+    month = int(data/100) %100
+    return    (year,month,day)
 def summarise(res,config):
-    port_series = res['sys_analyser']['portfolio']['static_unit_net_value']
-    
+    portfolio_series = res['sys_analyser']['portfolio']['static_unit_net_value']
+    market_series = res['sys_analyser']['benchmark_portfolio']['static_unit_net_value']
+    calculator = Cal()
+    alpha = calculator._alpha(portfolio_series,market_series)
+    annualized_returns = calculator._annualized_returns(portfolio_series)
+    benchmark_annualized_returns = calculator._annualized_returns(market_series)
+    benchmark_total_returns = calculator._total_returns(market_series)
+    max_drawdown = calculator._downside_risk(portfolio_series)
+    beta = calculator._downside_risk(portfolio_series,market_series)
+    sharpe = calculator._sharpe(portfolio_series)
+    total_returns = calculator._total_returns(portfolio_series)
+    volatility = calculator._var(portfolio_series)
+    start = config['start']
+    (year,month,day) = int_to_datetime(start)
+    start_datetime = datetime.datetime(year,month,day)
+    end = config['end']
+    (year,month,day) = int_to_datetime(end)
+    end_datetime = datetime.datetime(year,month,day) 
     summary = {
                'STOCK':config['initial_capital'],
-               'alpha':1.0,
-               'annualized_returns':1,
-               'benchmark_annualized_returns':1,
-               'benchmark_total_returns':1,
-               'beta':0.1,
-               'cash':1,
-               'downside_risk':1.1,
-               'end_date':datetime.datetime(2017,1,21),
-               'information_ratio':1.1,
-               'max_drawdown':0.1,
-               'run_type':'1',
-               'sharpe':1.2,
-               'sortino':1.1,
-                'start_date':datetime.datetime(2017,1,1),
+               'alpha':alpha,
+               'annualized_returns':annualized_returns,
+               'benchmark_annualized_returns':benchmark_annualized_returns,
+               'benchmark_total_returns':benchmark_total_returns,
+               'beta':beta,
+               'cash':config['initial_capital'],
+               'downside_risk':0.0,
+               'end_date':end_datetime,
+               'information_ratio':0,
+               'max_drawdown':max_drawdown,
+               'run_type':'0',
+               'sharpe':sharpe,
+               'sortino':0,
+                'start_date':start_datetime,
                 'strategy_file':'name',
                 'strategy_name':'name',
-                'total_returns':1,
-                'total_value':12,
-                'tracking_error':'123',
-                'unit_net_value':.1,
+                'total_returns':total_returns,
+                'total_value':total_returns,
+                'tracking_error':'0',
+                'unit_net_value':1.0,
                 'units':1,
-                'volatility':0.2
+                'volatility':volatility
             }
     res['sys_analyser']['summary'] = summary       
     return res
